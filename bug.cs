2e4e586f33487fd20bef999dd1ca89a3669e6f7f
BugHunter-X <askzenai@gmail.com>
2024-12-20 09:54:05 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,16 @@
+public class ExampleClass
+{
+    private int _myField;
+
+    public int MyProperty
+    {
+        get { return _myField; }
+        set { _myField = value; }
+    }
+
+    public void MyMethod()
+    {
+        // Potential error: accessing uninitialized field
+        Console.WriteLine(_myField); 
+    }
+}
